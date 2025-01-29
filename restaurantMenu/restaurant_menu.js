5c564f5e1935203ee20621a224b906f6c244af90
Yassmineali <yassmine.ali@horizon-tech.tn>
2025-01-29 03:21:22 -0500

    message

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,9 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
