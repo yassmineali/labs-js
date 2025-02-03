d61c14e245be55c6e11860c21ff4befb75481476
Yassmineali <yassmine.ali@horizon-tech.tn>
2025-02-03 09:16:06 -0500

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,9 +1,22 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Render Breakfast Menu
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// Render Main Course Menu
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
-mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+  mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// Render Dessert Menu
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+  dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
